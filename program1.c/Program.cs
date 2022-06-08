9c5e5fa6f4c1b71496d3cc033783d74a3b31bdf1
Luana Brenna Negreiro <luana.negreiro@fitbank.com.br>
2022-06-08 10:03:11 -0300

    First commit

---
+++ program1.c/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace program1.c
+{
+    class Program
+    {
+        static void Main()
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos: \n");
+            Console.WriteLine(produto1 + ", cujo preço é " + preco1);
+            Console.WriteLine(produto2+ ", cujo preço é "+ preco2);
+            Console.WriteLine("Registro: "+ idade+ " anos de idade, codigo"+ codigo+ " e gênero: "+ genero);
+            Console.WriteLine("Medida com oito casas decimais: "+ medida);
+            Console.WriteLine("Arredondado (três casas decimais): "+ string.Format("{0:0.000}", medida));
+            Console.WriteLine("Separador decimal invariant culture: "+ medida.ToString("F3", CultureInfo.InvariantCulture)); //ponto
+        }
+    }
+}
